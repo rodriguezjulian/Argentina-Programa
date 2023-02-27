514d53f47dfcf43c882e87c5dc0151ed5ecdcf4c
rodriguezjulian <julian.rodriguez.t2@gmail.com>
2023-02-27 12:56:55 -0300

    claseUno

---
+++ ClaseUno/claseUno.js
@@ -0,0 +1,26 @@
+// CLASE 1 EJERCICIO 1 
+let auto= "Corolla";
+console.log(auto);
+console.log(typeof(auto));
+
+//CLASE 1 EJERCICIO 2
+auto=308;
+console.log(auto);
+console.log(typeof(auto));
+
+//CLASE 1 EJERCICIO 3
+auto="C4";
+console.log(auto);
+console.log(typeof(auto));
+
+//CLASE 1 EJERCICIO 4
+let nombre="Julian Manuel";
+let apellido= " Rodriguez";
+console.log(nombre + apellido);
+
+//CLASE 1 EJERCICIO 5
+let fruta = "Naranja";
+let tipoDeLaVariableFruta= typeof(fruta);
+console.log(tipoDeLaVariableFruta);
+
+//CLASE 1 EJERCICIO 6 -- REALIZADO EN EL DESARROLLO
